1abfaacaa44b1b39ebdcf3c365237fe82dfdc843
Andreiow <jayadevahari96@gmail.com>
2024-07-25 19:04:36 -0300

    dress amker

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1721943293,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/body-sheet0.png",
+		"images/head-sheet0.png",
+		"images/hair-sheet0.png",
+		"images/eyes-sheet0.png",
+		"images/mouth-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"array.json"
+	]
+}
